e1bce71c10c9d8158bda96cd452876476f41918b
juan cardenas <juans.cardenasg@autonoma.edu>
2022-04-14 12:14:32 -0500

    seeder de rental availabilities

--- database/seeders/Rental_availabilitiesSeeder.php
+++ database/seeders/Rental_availabilitiesSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Rental_availability;
+
+
 class Rental_availabilitiesSeeder extends Seeder
 {
     /**
@@ -13,6 +18,28 @@ class Rental_availabilitiesSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $p = new Rental_availability();
+        $p->availability = true;
+        $p->start_date = date("2022-02-17");
+        $p->departure_date = date("2022-05-17");
+        $p->property_id = "1";
+        $p->save();
+
+        $p = new Rental_availability();
+        $p->availability = false;
+        $p->start_date = date("2022-06-17");
+        $p->departure_date = date("2022-010-17");
+        $p->property_id = "1";
+        $p->save();
+
+        $p = new Rental_availability();
+        $p->availability = true;
+        $p->start_date = date("2022-02-17");
+        $p->departure_date = date("2022-05-17");
+        $p->property_id = "2";
+        $p->save();
+
+
     }
 }
