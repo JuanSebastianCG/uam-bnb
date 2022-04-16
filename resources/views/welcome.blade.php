3a642b549af690b0906201598147150e94a4cafd
Manuel Patiño <manuel121938@gmail.com>
2022-04-16 02:25:31 -0500

    Se mejoró la vista "Welcome".

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <div class="row">
+        {{-- Título --}}
+        <div class="col s8">
+            <br><br>
+            <div class="row text-center">
+                <h3 style="color: black" class="text-dark display-3 ">
+                    UAM - BNB
+                </h3>
+            </div>
+            <br><br>
+            <div class="row text-center">
+                <h3 style="color: black" class="text-dark display-6 ">
+                    El hospedaje perfecto, en el lugar perfecto
+                </h3>
+            </div>
+            <br><br>
+
+            <img src="https://www.detectahotel.com/rimg/himg/91/26/a5/leonardo-5007061-Swimming_Pool_O-088786.jpg?width=1366&height=768&crop=true"
+            class="img-fluid" alt="..." width="">
+        </div>
+
+    </div>
+
+@endsection
 
 
 
