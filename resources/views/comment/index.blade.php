52226aed5b1ef02447fda63461408f40f4f22ec6
estebanpatinog <esteban.patinog@autonoma.edu.co>
2022-04-19 23:54:06 -0500

    Vista comentarios

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -7,25 +7,26 @@
     <h4>Comentarios</h4>
     @forelse ($comments as $comment)
         <div class="row">
-            <div class="col s4">
-                <div class="card">
-                    <center>
-                    <div class="card-header">
-                      Comentario
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">{{ $comment->text }}</p>
-                      @forelse ($properties as $property)
-                        @if ($property->id === $comment->property_id)
-                            <h5 class="card-title">{{ $property->name }}</h5>
-                        @endif
-                      @empty
+            <div class="col-s4">
+                <center>
+                    <div class="card" style="width: 24rem;">
+                        <div class="card-header">
+                        Comentario
+                        </div>
+                        <div class="card-body">
+                        <p class="card-text">{{ $comment->text }}</p>
+                        @forelse ($properties as $property)
+                            @if ($property->id === $comment->property_id)
+                                <h5 class="card-title">{{ $property->name }}</h5>
+                            @endif
+                        @empty
 
-                      @endforelse
-                      <a href="{{ route('properties.show', $comment->property_id) }}" class="btn btn-primary">Ir a la propiedad</a>
+                        @endforelse
+                        <a href="{{ route('properties.show', $comment->property_id) }}" class="btn btn-sm btn-primary">Ir a la propiedad</a>
+                        </div>
+
                     </div>
                 </center>
-                </div>
 
             </div>
         </div>
