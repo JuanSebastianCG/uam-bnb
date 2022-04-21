edf5cfdba27339561e6728dbdd3b87f479b99ef2
Santiago <s.jimenezv10@gmail.com>
2022-04-21 14:36:19 -0500

    Se agregó boton de comprar en la vista de show propiedades

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -97,6 +97,8 @@
     <div class="container mt-3 grayContainer">
         <h4 class="ml-3 mt-4 mb-4 " id="title"> Fechas disponibles</h4>
         @forelse($rental_availabilities as $rental_availability)
+        <form method="POST" action="{{ route('bills.store', $property->id, $user->id) }}" >
+            {{ csrf_field() }}
             <div class="ml-4 card border-dark mb-3 " id="grayCard" style="">
                 <div class="row">
                     <div class="card-body col-9">
@@ -108,16 +110,21 @@
                         <p class="card-text ml-4 " id="disponiblidadTrue" >Disponible</p>
                         @else
                         <p class="card-text ml-4" id="disponiblidadFalse"> No Disponible</p>
-
                         @endif
                         </div>
+                    </div>
+                    <div class="card-body col-2">
+                        <center>
+                            <button type="submit" class="btn btn-success btn-ls">
+                                {{ __('Comprar') }}
+                            </button>
+                        </center>
 
                     </div>
-
-                    <button type="button" class="btn btn-primary btn-lg col-auto mr-4" id="grayButton" >Comprar</button>
                 </div>
-
             </div>
+
+        </form>
         @empty
             <div class="card mb-2">
                 <div class="alert alert-info" role="alert">
