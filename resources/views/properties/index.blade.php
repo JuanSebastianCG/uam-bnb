5179391b6f9d9bdbea7f7516636d25942afe0b97
juan cardenas <juans.cardenasg@autonoma.edu>
2022-04-21 16:32:55 -0500

    cambio de nav-bar

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -1,75 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.subview-form-errors')
+    @include('layouts.subview-form-errors')
 
 
 
-<!-- side var -->
-<div id="wrapper" class="toggled nav">
-    <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-
-
-            <li>
-                <h2 id='navTitle'>Filtros</h2>
-            </li>
-
-            <li>
-                <label for="startDate" class="navLavel form-label mt-4">Fecha de Inicio</label>
-                <input name="startDate" id="startDate" type="date" class="navbarInput form-control" placeholder="fecha de inicio"/>
-            </li>
-
-            <li>
-                <label for="exampleSelect1" class="navLavel form-label mt-4">Fecha final</label>
-                <input name="endDate" id="endDate" type="date" class="navbarInput form-control" placeholder="fecha de inicio"/>
-            </li>
-
-
-            <li>
-                <div class="form-group">
-                    <label for="exampleSelect1" class="navLavel form-label mt-4">Precio</label>
-                    <select class="navbarInput form-select" id="price">
-                        <option selected value="default">Elegir orden</option>
-                        <option value="asc">Mayor Precio</option>
-                        <option value="desc">Menor precio</option>
-
-                    </select>
-                </div>
-            </li>
-
-            <li>
-                <div class="form-group">
-                    <label class="col-form-label navLavel " for="inputDefault">Ciudad</label>
-                    <input type="text" class="navbarInput form-control" placeholder="Default input" id="city">
-                </div>
-            </li>
-
-            <li>
-                <div class="form-group">
-                    <button type="button" class="btn btn-outline-light" id="filter">Filtrar</button>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<!-- propiedades -->
-<div class="container indexContainer">
-    <h4 >Propiedades de {{ $user->name}}</h4>
+    <!-- side var -->
     <div class="row">
-     @for($i = 0; $i < count($properties); $i++)
-        @include('properties.subview-index')
+        <div class="nav-var col-auto ">
 
-      @endfor
+            <h2 class='navTitle'>Filtros</h2>
+
+            <label for="startDate" class="navLavel form-label mt-4">Fecha de Inicio</label>
+            <input name="startDate" id="startDate" type="date" class="navbarInput form-control"
+                placeholder="fecha de inicio" />
+
+
+            <label for="exampleSelect1" class="navLavel form-label mt-4">Fecha final</label>
+            <input name="endDate" id="endDate" type="date" class="navbarInput form-control" placeholder="fecha de inicio" />
+
+
+            <div class="form-group">
+                <label for="exampleSelect1" class="navLavel form-label mt-4">Precio</label>
+                <select class="navbarInput form-select" id="price">
+                    <option selected value="default">Elegir orden</option>
+                    <option value="asc">Mayor Precio</option>
+                    <option value="desc">Menor precio</option>
+
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label class="col-form-label navLavel " for="inputDefault">Ciudad</label>
+                <input type="text" class="navbarInput form-control" placeholder="Default input" id="city">
+            </div>
+
+            <div class="form-group">
+                <button type="button" class="btn btn-outline-light" id="filter">Filtrar</button>
+            </div>
+
+        </div>
+
+        <!-- propiedades -->
+        <div class="indexContainer col-8">
+            <h4 class="mb-5">Propiedades de {{ $user->name }}</h4>
+            <div class="row">
+                @for ($i = 0; $i < count($properties); $i++)
+                    @include('properties.subview-index')
+                @endfor
+            </div>
+        </div>
+
     </div>
-</div>
-
-
-@include('layouts.sweetalert')
-@include('properties.filterIndex')
-
-
-
-
+    @include('layouts.sweetalert')
+    @include('properties.filterIndex')
 @endsection
--- resources/views/properties/subview-index.blade.php
+++ resources/views/properties/subview-index.blade.php
@@ -1,27 +1,26 @@
+<div class="col-sm-3 " id="cartas">
+    <div class="card text-white bg-primary mb-3 "  style="width: 23rem;">
+        <a target="_blank" href="{{ route('properties.show', $properties[$i], $user) }}" style="flex: 1;">
 
-   <div class="col-sm-3 m-5 " >
-       <div class="card text-white bg-primary mb-3" style="width: 23rem;">
-        <a  target="_blank" href="{{ route('properties.show', $properties[$i], $user) }}" style="flex: 1;">
 
-
-        @if($photos[$i] != null)
-                <img class="card-img-top"  height="300px" src="/property_images/{{$photos[$i]->url_image}}" alt="Card image cap">
-        @else
-        <img class="card-img-top"  height="300px" src="/property_images/defaultImage.jpg" alt="Card image cap">
-        @endif
-         <blockquote class="blockquote">
-            <h4 class="text-white"><p class="ml-3 mt-3">{{ $properties[$i]->name }}</p></h4>
-        </blockquote>
+            @if ($photos[$i] != null)
+                <img class="card-img-top" height="300px" src="/property_images/{{ $photos[$i]->url_image }}"
+                    alt="Card image cap">
+            @else
+                <img class="card-img-top" height="300px" src="/property_images/defaultImage.jpg" alt="Card image cap">
+            @endif
+            <blockquote class="blockquote">
+                <h4 class="text-white">
+                    <p class="ml-3 mt-3">{{ $properties[$i]->name }}</p>
+                </h4>
+            </blockquote>
 
             <div class="card-body">
 
-                    <p class="card-text text-white"> Tipo de propiedad: {{ __($properties[$i]->type) }}</p>
-                    <p class="card-text text-white"> Nombre: {{ $properties[$i]->name }}</p>
+                <p class="card-text text-white"> Tipo de propiedad: {{ __($properties[$i]->type) }}</p>
+                <p class="card-text text-white"> Nombre: {{ $properties[$i]->name }}</p>
 
-                </div>
-            </a>
-        </div>
+            </div>
+        </a>
     </div>
-
-
-
+</div>
