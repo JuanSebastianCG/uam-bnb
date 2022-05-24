b12a80831bba3da46eee5f8db5d9ec95f7ec8fff
Manuel Patiño <manuel121938@gmail.com>
2022-05-24 16:30:24 -0500

    Se agregó el Hash en el update de User desde la API.

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\api\v1\UserResource;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -52,8 +52,15 @@ class UserController extends Controller
      */
     public function update(Request $request)
     {
+
         $user = auth('sanctum')->user();
-        $user->update($request->all());
+        if($request->password != null){
+            $user->update($request->all());
+            $user->password = Hash::make($request->password);
+        }else{
+            $user->update($request->all());
+        }
+
         return response()->json(['data' => $user], 200);
     }
 
